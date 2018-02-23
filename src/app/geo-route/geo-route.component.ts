daccf5514cb40ac1feb1099fd7085723aa5c2107
Imran Syed <syed.imran2512@gmail.com>
2018-02-22 22:41:25 -0500

    done

--- src/app/geo-route/geo-route.component.ts
+++ src/app/geo-route/geo-route.component.ts
@@ -215,7 +215,7 @@ export class GeoRouteComponent implements OnInit {
             } else if (status == 'UNKNOWN_ERROR') {
               console.log('A directions request could not be processed due to a server error. The request may succeed if you try again.');
             } else if (status == 'REQUEST_DENIED') {
-              console.log('This webpage is not allowed to use the directions service.');
+              console.log('This webpage is not allowed to use the directions service and service is rejected.');
             } else if (status == 'OVER_QUERY_LIMIT') {
               console.log('The webpage has gone over the requests limit in too short a period of time.');
             } else if (status == 'NOT_FOUND') {
