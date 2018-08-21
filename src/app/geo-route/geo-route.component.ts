4a4fac9d1b4bec9484d5f3d5ff60a2a3029865c4
Imran Syed <syed.imran2512@gmail.com>
2018-08-20 22:06:52 -0400

    finish

--- src/app/geo-route/geo-route.component.ts
+++ src/app/geo-route/geo-route.component.ts
@@ -151,7 +151,7 @@ export class GeoRouteComponent implements OnInit {
           console.log('routes and their distances are good to go with thier places');
           that.control++;
           if (status === 'OKAY') {
-           console.log('routes are OKAY and are ready to go and find the respective places');
+           console.log('routes are OKAY and are ready to go and find their respective places');
             showRoutesMap = responseAll
             var route = response.routes[0];
             let i = 0;
